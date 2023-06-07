9f49dc7084fce724746411b527dc8e7819621ace
Victor Ike <victorike08@gmail.com>
2023-06-07 01:07:32 -0500

    Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
-      signature: ['Great Vibes'],
+      signature: ["Great Vibes"],
+    },
+    colors: {
+      primary: "#0d2240",
+      offblue: "#A9A9AF",
+      accent: " #f7a800",
+      info: "#00a8e1",
+      white: "#fbfbf8",
+      gray: "#F8F8F8",
+      html: "#f76336",
     },
   },
   plugins: [],
