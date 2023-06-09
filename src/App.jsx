4e096d4da55f842a1426d5277320d0d9092026a5
Victor Ike <victorike08@gmail.com>
2023-06-08 19:12:54 -0500

    Fix error on build

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import SocialLinks from "./components/SocialLinks";
-import Portfolio from "./components/portfolio";
+import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
