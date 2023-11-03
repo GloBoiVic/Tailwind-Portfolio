259617ed1df53c8c64b08e4e3aa71532c5e31d06
GloBoiVic <victorike08@gmail.com>
2023-11-03 17:04:00 -0500

    Update data

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,45 +1,48 @@
 export const NavLinks = [
-	{
-		id: 1,
-		link: 'home',
-	},
-	{
-		id: 2,
-		link: 'portfolio',
-	},
-	{
-		id: 3,
-		link: 'experience',
-	},
-	{
-		id: 4,
-		link: 'contact',
-	},
+  {
+    id: 1,
+    link: 'home',
+  },
+  {
+    id: 2,
+    link: 'portfolio',
+  },
+  {
+    id: 3,
+    link: 'experience',
+  },
+  {
+    id: 4,
+    link: 'contact',
+  },
 ];
 
 export const portfolioData = [
-	{
-		id: 1,
-		src: 'picture-my-code.jpg',
-		demo: 'https://globoivic.github.io/Etch-A-Sketch/',
-		code: 'https://github.com/GloBoiVic/Wordit',
-	},
-	{
-		id: 2,
-		src: 'picture-my-code.jpg',
-		demo: 'https://globoivic.github.io/JS-Todo-List/',
-		code: 'https://github.com/GloBoiVic/JS-Todo-List',
-	},
-	{
-		id: 3,
-		src: 'picture-my-code.jpg',
-		demo: 'https://globoivic.github.io/Tesla-Clone/',
-		code: 'https://github.com/GloBoiVic/Tesla-Clone',
-	},
-	{
-		id: 4,
-		src: 'picture-my-code.jpg',
-		demo: 'https://globoivic.github.io/Calculator-App/',
-		code: 'https://github.com/GloBoiVic/Calculator-App',
-	},
+  {
+    id: 1,
+    name: 'Wordit',
+    description: 'Keep track of your words with this custom vocabulary tracker app',
+    image: 'picture-my-code.jpg',
+    svg: 'typescript.svg',
+    demo: 'https://globoivic.github.io/Etch-A-Sketch/',
+    code: 'https://github.com/GloBoiVic/Wordit',
+  },
+  {
+    id: 2,
+    name: 'The Wild Oasis',
+    description: 'A full-stack dashboard app for a hotel company',
+    image: 'picture-my-code.jpg',
+    svg: 'javascript.svg',
+    demo: 'https://globoivic.github.io/JS-Todo-List/',
+    code: 'https://github.com/GloBoiVic/JS-Todo-List',
+  },
+  {
+    id: 3,
+    name: 'Picture My Code',
+    description: 'A code snippet sharing app for developers',
+    svg: 'javascript.svg',
+    image: 'picture-my-code.jpg',
+    demo: 'https://globoivic.github.io/Tesla-Clone/',
+    code: 'https://github.com/GloBoiVic/Tesla-Clone',
+  },
 ];
