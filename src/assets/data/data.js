644ce3276e961e315d961b404e57a45f6c0f409b
GloBoiVic <victorike08@gmail.com>
2023-11-03 12:45:24 -0500

    Update data

---
+++ src/assets/data/data.js
@@ -0,0 +1,45 @@
+export const NavLinks = [
+	{
+		id: 1,
+		link: 'home',
+	},
+	{
+		id: 2,
+		link: 'portfolio',
+	},
+	{
+		id: 3,
+		link: 'experience',
+	},
+	{
+		id: 4,
+		link: 'contact',
+	},
+];
+
+export const portfolioData = [
+	{
+		id: 1,
+		src: 'picture-my-code.jpg',
+		demo: 'https://globoivic.github.io/Etch-A-Sketch/',
+		code: 'https://github.com/GloBoiVic/Wordit',
+	},
+	{
+		id: 2,
+		src: 'picture-my-code.jpg',
+		demo: 'https://globoivic.github.io/JS-Todo-List/',
+		code: 'https://github.com/GloBoiVic/JS-Todo-List',
+	},
+	{
+		id: 3,
+		src: 'picture-my-code.jpg',
+		demo: 'https://globoivic.github.io/Tesla-Clone/',
+		code: 'https://github.com/GloBoiVic/Tesla-Clone',
+	},
+	{
+		id: 4,
+		src: 'picture-my-code.jpg',
+		demo: 'https://globoivic.github.io/Calculator-App/',
+		code: 'https://github.com/GloBoiVic/Calculator-App',
+	},
+];
