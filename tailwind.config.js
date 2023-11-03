86745474fe4c2449e77e8803157b6a45cf5ae8f7
GloBoiVic <victorike08@gmail.com>
2023-11-03 17:03:46 -0500

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        primary: "#0d2240",
-        offblue: "#A9A9AF",
-        accent: " #f7a800",
-        info: "#00a8e1",
-        offwhite: "#fbfbf8",
-        offgray: "#F8F8F8",
+        primary: '#0d2240',
+        offblue: '#A9A9AF',
+        accent: ' #f7a800',
+        info: '#00a8e1',
+        offwhite: '#fbfbf8',
+        offgray: '#F8F8F8',
+      },
+      height: {
+        screen: '100dvh',
       },
     },
   },
